f6ac116779e64f71b34d7ee30a453ebc082e1401
Jordan Cooper <jordancooper1195@gmail.com>
2022-10-23 14:21:44 +1300

    Initial .ts express setup

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (_, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(port, () => {
+  const timestamp: String = new Date().toLocaleString();
+  console.log(`Server is listening on port: ${port} at ${timestamp}`);
+});
+
+// closes port gracefully
+process.on('SIGINT', () => {
+  const timestamp: String = new Date().toLocaleString();
+  console.log(`\nServer is closing down port: ${port} at ${timestamp}`);
+  process.exit(0);
+});
